f8398e1e9dae13d9942c3a4b841a3e7802be1397
Chinedu Ugbo <o.ugbochinedu@yahoo.com>
2023-08-24 10:58:25 +0100

    Updated MediaService and UserController

--- src/main/java/africa/semicolon/promeescuous/repositories/MediaRepository.java
+++ src/main/java/africa/semicolon/promeescuous/repositories/MediaRepository.java
@@ -4,4 +4,8 @@ import africa.semicolon.promeescuous.models.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MediaRepository extends JpaRepository<Media, Long> {
+    boolean existsByUser(User user);
+
+    @Query("SELECT m FROM Media m WHERE m.user = :user AND m.isLike = true")
+    Optional<Media> findMediaByUserAndIsLikeIsTrue(User user);
 }
